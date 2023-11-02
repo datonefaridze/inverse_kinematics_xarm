c27d910f804c55c8d85b65f92ac397becd797435
datonefaridze <dane@odoo.com>
2023-11-02 20:42:22 +0100

    finally working code

--- main.py
+++ main.py
@@ -12,22 +12,21 @@ def move():
     np.set_printoptions(precision=2, suppress=True, formatter={'float': '{:0.2f}'.format})
 
     # x, y, z
-    target_position = [0.03, 0.03, 0.25]
+    target_position = [0.20, 0, 0.25]
     my_chain = ikpy.chain.Chain.from_urdf_file("xarm.URDF")
     print('my_chain: ', my_chain)
     target_angles = my_chain.inverse_kinematics(target_position)
 
-    # target_angles = np.insert(target_angles, 0, 0., axis=0)
     target_angles_degrees = np.array([math.degrees(radian) for radian in target_angles])
     print("The angles of each joints are : ", target_angles_degrees)
-    target_angles_degrees =  np.flip(target_angles_degrees[1:])
+    target_angles_degrees =  np.flip(target_angles_degrees[1:-1])
     print(target_angles_degrees)
 
 
-    real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_position))
-    print("Computed vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
+    # real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_position))
+    # print("Computed vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
 
-    desired_pos = [[x+2, float(target_angles_degrees[x])] for x in range(len(target_angles_degrees))]
+    desired_pos = [[x+3, float(target_angles_degrees[x])] for x in range(len(target_angles_degrees))]
     arm.setPosition(desired_pos, duration=1000, wait=True)
 
 if __name__ == '__main__':
